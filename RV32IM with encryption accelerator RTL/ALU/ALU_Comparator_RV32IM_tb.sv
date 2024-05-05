fa802bf7e3226b6cf1d02868aad6869cd075685a
Reem Ahmad <83836690+reem298@users.noreply.github.com>
2024-05-05 19:33:40 +0300

    Update ALU_Comparator_RV32IM_tb.sv

--- RV32IM with encryption accelerator RTL/ALU/ALU_Comparator_RV32IM_tb.sv
+++ RV32IM with encryption accelerator RTL/ALU/ALU_Comparator_RV32IM_tb.sv
@@ -37,14 +37,45 @@ module ALU_Comparator_Testbench;
     $display("Test case 1: operand_A = %d, operand_B = %d", operand_A, operand_B);
     $display("Greater = %b, Equal = %b, Less = %b", Greater, Equal, Less);
 
-    // Test case 2: randomized test
- 
-   repeat(1000)begin
+
+
+    // Test case 2: directed test
+    operand_A = -655;
+    operand_B = 3;
+    #10;
+    $display("Test case 2: operand_A = %d, operand_B = %d", operand_A, operand_B);
+    $display("Greater = %b, Equal = %b, Less = %b", Greater, Equal, Less);
+
+
+    // Test case 3: directed test
+    operand_A = 255;
+    operand_B = -343;
+    #10;
+    $display("Test case 3: operand_A = %d, operand_B = %d", operand_A, operand_B);
+    $display("Greater = %b, Equal = %b, Less = %b", Greater, Equal, Less);
+
+
+    // Test case 4: directed test
+    operand_A = -11;
+    operand_B = -346;
+    #10;
+    $display("Test case 4: operand_A = %d, operand_B = %d", operand_A, operand_B);
+    $display("Greater = %b, Equal = %b, Less = %b", Greater, Equal, Less);
+
+
+    // Test case 5: directed test
+    operand_A = 8995;
+    operand_B = 5433;
+    #10;
+    $display("Test case 5: operand_A = %d, operand_B = %d", operand_A, operand_B);
+    $display("Greater = %b, Equal = %b, Less = %b", Greater, Equal, Less);
+
+    // Test case 6: randomized test
      operand_A=$random;
      operand_B=$random;
        #10;
-    $display("Test case 1: operand_A = %d, operand_B = %d", operand_A, operand_B);
+    $display("Test case 6: operand_A = %d, operand_B = %d", operand_A, operand_B);
     $display("Greater = %b, Equal = %b, Less = %b", Greater, Equal, Less);  
-   end   
-
+ 
+end
 endmodule
